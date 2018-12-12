2263416b483bd33e62a4f0f8a23223fec6a2572b
Alan Tavares de Melo <alantmelo@gmail.com>
2018-12-12 17:50:57 -0300

    Products pagination

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -4,24 +4,49 @@ import './styles.css';
 
 export default class Main extends Component {
     state = {
-        products:[]
+        products: [],
+        productInfo: {},
+        page: 1
     }
 
     componentDidMount(){
         this.loadProduct();
     }
 
-    loadProduct = async () => {
-        const response = await api.get('/products');
-        console.log(response); 
-        this.setState({products: response.data.docs});
+    loadProduct = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
+
+        const { docs, ...productInfo } = response.data;
+
+        this.setState({products: docs, productInfo, page});
     };
+
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        console.log(productInfo.page);
+        
+        if(page === productInfo.page) return
+
+        const pageNumber = page + 1;
+
+        this.loadProduct(pageNumber);
+    }
+    
+    prevPage = () => {
+        const { page, productInfo } = this.state;
+        
+        if(page === 1) return;
+
+        const pageNumber = page - 1;
+        this.loadProduct(pageNumber);
+    }
 
     render(){
         const {products} = this.state;
         return (
             <div className="products-list">
-                <h1>Number of Products {products.length}</h1>
+                {/* <h1>Number of Products {this.state.total}</h1> */}
                 {products.map(product => (
                     <article key={product._id}>
                         <strong>{product.title}</strong>
@@ -29,7 +54,11 @@ export default class Main extends Component {
                         <a href="">Acessar</a>
                     </article>
                 ))}
+                <div className="actions">
+                    <button onClick={this.prevPage}>Back</button>
+                    <button onClick={this.nextPage}>Forward</button>
+                </div>
             </div>
         )
-    }
+    };
 }
