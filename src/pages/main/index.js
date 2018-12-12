3ef4b26bd5409b7ecd90cd5e03ff62b437cd54b2
Alan Tavares de Melo <alantmelo@gmail.com>
2018-12-12 16:46:09 -0300

    product list style

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
 import api from './../../services/api';
+import './styles.css';
 
 export default class Main extends Component {
-    
+    state = {
+        products:[]
+    }
+
     componentDidMount(){
         this.loadProduct();
     }
@@ -10,9 +14,22 @@ export default class Main extends Component {
     loadProduct = async () => {
         const response = await api.get('/products');
         console.log(response); 
+        this.setState({products: response.data.docs});
     };
 
     render(){
-        return <h1>Hello World</h1>
+        const {products} = this.state;
+        return (
+            <div className="products-list">
+                <h1>Number of Products {products.length}</h1>
+                {products.map(product => (
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+                        <a href="">Acessar</a>
+                    </article>
+                ))}
+            </div>
+        )
     }
 }
